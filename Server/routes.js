74e8394945303400d8f2a6697c4f9fac7a69053e
aronresni <aronresni@gmail.com>
2023-07-18 18:50:45 -0300

    prueba de productos

--- Server/routes.js
+++ Server/routes.js
@@ -5,25 +5,62 @@ const router = express.Router();
 const productos = [
     {
         id: 1,
-        nombre: "Proteina Ultra Mass",
-        precio: 10999,
+        image: [
+            "https://getrawnutrition.com/cdn/shop/files/RAW-3_720x.jpg?v=1684901900",
+        ],
+        nombre: "CBUM ITHOLATE PROTEIN",
+        precio: 54.99,
+        imageinfo: "https://cdn.shopify.com/s/files/1/0303/1135/0365/files/VANILLA_OATMEAL_COOKIE_480x480.jpg?v=1688397970",
         tamaño: "1.5 Kg",
         categoria: "Suplementos",
     },
     {
-        id: 2,
-        nombre: "Proteina Ultra Mass",
-        precio: 10999,
+        id: 1,
+        image: [
+            "https://getrawnutrition.com/cdn/shop/files/RAW-3_720x.jpg?v=1684901900",
+        ],
+        nombre: "CBUM ITHOLATE PROTEIN",
+        precio: 54.99,
+        imageinfo: "https://cdn.shopify.com/s/files/1/0303/1135/0365/files/VANILLA_OATMEAL_COOKIE_480x480.jpg?v=1688397970",
         tamaño: "1.5 Kg",
         categoria: "Suplementos",
     },
     {
-        id: 3,
-        nombre: "Proteina Ultra Mass",
-        precio: 10999,
+        id: 1,
+        image: [
+            "https://getrawnutrition.com/cdn/shop/files/RAW-3_720x.jpg?v=1684901900",
+        ],
+        nombre: "CBUM ITHOLATE PROTEIN",
+        precio: 54.99,
+        imageinfo: "https://cdn.shopify.com/s/files/1/0303/1135/0365/files/VANILLA_OATMEAL_COOKIE_480x480.jpg?v=1688397970",
         tamaño: "1.5 Kg",
         categoria: "Suplementos",
-    }
+    },
+    {
+        id: 1,
+        image: [
+            "https://getrawnutrition.com/cdn/shop/files/RAW-3_720x.jpg?v=1684901900",
+        ],
+        nombre: "CBUM ITHOLATE PROTEIN",
+        precio: 54.99,
+        imageinfo: "https://cdn.shopify.com/s/files/1/0303/1135/0365/files/VANILLA_OATMEAL_COOKIE_480x480.jpg?v=1688397970",
+        tamaño: "1.5 Kg",
+        categoria: "Suplementos",
+    },
+    {
+        id: 1,
+        image: [
+            "https://getrawnutrition.com/cdn/shop/files/6_0231e27f-de59-4bbb-9a71-f53bd72f6d81_720x.png?v=1688147354",
+            "https://getrawnutrition.com/cdn/shop/files/7_a9c795a5-316a-4ee3-af5f-54bacfb2644d_720x.png?v=1688147354",
+        ],
+        nombre: "CBUM ITHOLATE PROTEIN",
+        precio: 54.99,
+        imageinfo: "https://cdn.shopify.com/s/files/1/0303/1135/0365/files/VANILLA_OATMEAL_COOKIE_480x480.jpg?v=1688397970",
+        tamaño: "1.5 Kg",
+        categoria: "Suplementos",
+    },
+
+
 ]
 
 // Ruta para obtener todos los productos
