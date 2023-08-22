ad7977cf5cddf9e89537f21fb321fc721883e7fc
aronresni <aronresni@gmail.com>
2023-08-22 16:55:26 -0300

    upload footer error

--- Focus-First/src/Pages/Cart.jsx
+++ Focus-First/src/Pages/Cart.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import NavBar from '../components/NavBar/NavBar';
-import Footer from "../components/Footer/Footer"
-import  CartContent from "../components/CartContent/index" 
-import { CartContext } from '../state/cart.context';
+import Footer from "../components/Footer/footer"
+import CartContent from "../components/CartContent/index"
+
 
 const Cart = () => {
     return (
         <div>
             <NavBar />
-            <CartContent/>
+            <CartContent />
             <Footer />
         </div>
     )
